59208b5951bf913c8493637a5cb8f44c4824718f
DJ Papzin <L.fanampe@gmail.com>
2023-09-19 13:33:16 +0200

    Initial commit: Movie Info Finder using LangChain and OpenAI

---
+++ README.md
@@ -0,0 +1,51 @@
+# Movie Info Finder
+
+Discover detailed insights about movies using the power of OpenAI's `gpt-3.5-turbo` model. Whether you're curious about the plot, cast, or any other details, our assistant is here to help.
+
+## Setup
+
+### Running Locally
+
+1. **Environment Variables**: Rename the `.env copy` file to `.env`.
+   
+2. **API Key**:
+   - Obtain your OpenAI API key from [OpenAI Platform](https://platform.openai.com/account/api-keys) and input it into the `.env` file.
+
+3. **Requirements**: Install the necessary packages using:
+   ```
+   pip install -r requirements.txt
+   ```
+
+### Running on Streamlit
+
+1. **Secrets**: Rename the `secrets.toml copy` file inside the `.streamlit` folder to `secrets.toml`.
+
+2. **API Key**:
+   - Obtain your OpenAI API key from [OpenAI Platform](https://platform.openai.com/account/api-keys) and input it into the `secrets.toml` file.
+
+3. **Requirements**: Install the necessary packages using:
+   ```
+   pip install -r requirements.txt
+   ```
+
+## Running the App
+
+### Locally
+
+To run the app locally, use the following command:
+
+```
+python movie_info_finder.py
+```
+
+### Streamlit
+
+To run the app on Streamlit, use:
+
+```
+streamlit run movie_info_finder.py
+```
+
+## Acknowledgments
+
+This project was inspired and developed by following the ActiveLoop tutorial on [Building Applications Powered by LLMs with LangChain](https://learn.activeloop.ai/courses/take/langchain/multimedia/46317658-building-applications-powered-by-llms-with-langchain).
---
+++ movie_info_finder.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import sys
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+# Initialize the chat model
+chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+# Define the system message to set the context for the AI
+template = "You are an assistant that helps users find information about movies."
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+
+# Define the human message template to get information about a specific movie
+human_template = "Find information about the movie {movie_title}."
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+# Combine the system and human message templates to create the chat prompt
+chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+def get_movie_info(movie_title):
+    # Format the chat prompt with the user's movie title and convert it to messages
+    response = chat(chat_prompt.format_prompt(movie_title=movie_title).to_messages())
+    return response.content
+
+def run_streamlit_app():
+    st.title("Movie Info Finder")
+    st.write("Powered by OpenAI's `gpt-3.5-turbo` model, our Movie Info Finder provides insights into movies, including plot summaries and cast details. Dive into cinematic archives with just a movie title and experience the capabilities of advanced language models firsthand!")
+    
+    # Using markdown for italic text
+    st.markdown("*Disclaimer: Movie information is limited up to September 2021.*")
+    
+    # Using the label parameter for the input box
+    movie_title = st.text_input(label="Movie Title", value="", label_visibility="hidden")
+    
+    if st.button("Find"):
+        with st.spinner('Finding movie information...'):
+            result = get_movie_info(movie_title)
+        st.write(result)
+
+def run_terminal_app():
+    movie_title = input("Enter the title of the movie you want information about: ")
+    result = get_movie_info(movie_title)
+    print(result)
+
+if __name__ == "__main__":
+    # Check if the script is being run through Streamlit or not
+    if 'streamlit' in sys.modules:
+        run_streamlit_app()
+    else:
+        run_terminal_app()
